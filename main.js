2a68f710c6d4e33e5392b2ca143061fb36fbc744
AGAPPLICATIONS <90456136+AGAPPLICATIONS@users.noreply.github.com>
2023-05-31 12:55:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+
+function preload(){
+
+}
+function setup(){
+canvas=createCanvas(450,300);
+canvas.position(420,230);
+
+}
+function draw(){
+
+}
+function music(){
+    var x = document.getElementById("lol");
+    x.play();
+}
